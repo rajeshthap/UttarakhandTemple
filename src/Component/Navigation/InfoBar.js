24a8e39fdf39ef5f9cda33843e0597145ddb828a
Deepika <deepikachauhan8988@gmail.com>
2025-08-28 14:04:43 +0530

    UI fixed

--- src/Component/Navigation/InfoBar.js
+++ src/Component/Navigation/InfoBar.js
@@ -1,64 +1,87 @@
-import React from 'react'
-import { Col, Row } from 'react-bootstrap'
+import React from "react";
+import { Col, Row } from "react-bootstrap";
 import "../../assets/CSS/TopInfo.css"; // Importing the CSS file for styling
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
 import { IoKeySharp } from "react-icons/io5";
 import { FaUserGroup } from "react-icons/fa6";
 
 const InfoBar = () => {
-    return (
-        <>
-            <div className="header-top container-fluid">
-                <Row className="align-items-center">
-                    {/* Left Side */}
-                    <Col xs={12} md={6} lg={6} className="text-center info-mob text-md-start d-flex flex-wrap align-items-center">
-                        <div className="px-3">
-                            <i className="bi bi-telephone-fill infoicon"></i> +91 1234567890
-                        </div>
-                        <div className="px-3">
-                            <i className="bi bi-envelope-fill infoicon"></i> info@gmail.com
-                        </div>
-                    </Col>
-
-                    {/* Right Side */}
-                    <Col xs={12} md={6} lg={6} className="text-center text-md-end">
-                        <div className="d-inline-flex align-items-center flex-wrap justify-content-center">
-                            <Dropdown className="px-2">
-                                <Dropdown.Toggle variant="" className="login-btn" id="login-dropdown">
-                                    <span className="top-icon"><IoKeySharp /></span> Login
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item href="#/action-1">Admin</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-2">Temple Authority</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-3">Pandit ji </Dropdown.Item>
-                                    <Dropdown.Item href="#/action-3">Devotee</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-
-                            <Dropdown className="px-2">
-                                <Dropdown.Toggle variant="" className="register-btn" id="register-dropdown">
-                                    <span className="top-icon"><FaUserGroup /></span> Register
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item href="/TempleAuthority">Temple Authority</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-3">Pandit ji </Dropdown.Item>
-                                    <Dropdown.Item href="#/action-3">Devotee</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-
-                            <div className="px-3 d-flex gap-2 social-icon">
-                                <i className="bi bi-facebook infoicon"></i>
-                                <i className="bi bi-twitter infoicon"></i>
-                                <i className="bi bi-instagram infoicon"></i>
-                                <i className="bi bi-linkedin infoicon"></i>
-                                <i className="bi bi-youtube infoicon"></i>
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
+  return (
+    <>
+      <div className="header-top container-fluid">
+        <Row className="align-items-center">
+          {/* Left Side */}
+          <Col
+            xs={12}
+            md={6}
+            lg={6}
+            className="text-center info-mob text-md-start d-flex flex-wrap align-items-center"
+          >
+            <div className="px-3">
+              <i className="bi bi-telephone-fill infoicon"></i> +91 1234567890
             </div>
-        </>
-    )
-}
+            <div className="px-3">
+              <i className="bi bi-envelope-fill infoicon"></i> info@gmail.com
+            </div>
+          </Col>
 
-export default InfoBar
+          {/* Right Side */}
+          <Col xs={12} md={6} lg={6} className="text-center text-md-end">
+            <div className="d-inline-flex align-items-center flex-wrap justify-content-center">
+              <Dropdown className="px-2">
+                <Dropdown.Toggle
+                  variant=""
+                  className="login-btn"
+                  id="login-dropdown"
+                >
+                  <span className="top-icon">
+                    <IoKeySharp />
+                  </span>{" "}
+                  Login
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Admin</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Temple Authority
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Pandit ji </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Devotee</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+              <Dropdown className="px-2">
+                <Dropdown.Toggle
+                  variant=""
+                  className="register-btn"
+                  id="register-dropdown"
+                >
+                  <span className="top-icon">
+                    <FaUserGroup />
+                  </span>{" "}
+                  Register
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="/TempleAuthority">
+                    Temple Authority
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Pandit ji </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Devotee</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+              <div className="px-3 d-flex gap-2 social-icon">
+                <i className="bi bi-facebook infoicon"></i>
+                <i className="bi bi-twitter infoicon"></i>
+                <i className="bi bi-instagram infoicon"></i>
+                <i className="bi bi-linkedin infoicon"></i>
+                <i className="bi bi-youtube infoicon"></i>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+};
+
+export default InfoBar;
--- src/Component/userregistration/TempleAuthority.js
+++ src/Component/userregistration/TempleAuthority.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import "../../assets/CSS/TempleAuthority.css";
 
 function TempleAuthority() {
   return (
-    <div>TempleAuthority</div>
-  )
+    <div>
+      <Container className="temp-container">
+        <div>
+          <div className="mt-3 temple-registration-heading p-3">
+            <h1>Temple Registration</h1>
+            <div>
+              <Form>
+                <Row className="mt-4">
+                  <Col lg={4} md={4} sm={4}>
+                    <Form.Group
+                      className="mb-3"
+                      controlId="exampleForm.ControlInput1"
+                    >
+                      <Form.Label className="temp-label">
+                        Temple Name <span className="temp-span-star">*</span>
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder=""
+                        className="temp-form-control"
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
 
-export default TempleAuthority
+export default TempleAuthority;
