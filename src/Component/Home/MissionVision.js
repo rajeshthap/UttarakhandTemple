0482bfc741aec9012e7f41d2852a66e59c184e4b
rajeshthap <jdmjvm@gmail.comgit config user.email>
2025-09-15 14:34:17 +0530

    Mission & Vision UI done

--- src/Component/Home/MissionVision.js
+++ src/Component/Home/MissionVision.js
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import { Col, Container, Row } from 'react-bootstrap'
+import Missionimg from "../../assets/images/Mission-img.png";
 function MissionVision() {
   return (
-    <div>MissionVision</div>
+    <div>
+      <Container className='temp-container p-4'>
+        <Row>
+          <Col lg={7} md={7} sm={12}>
+
+            <h1>Mission & Vision</h1>
+            <h2>Mission</h2>
+            <p>Our mission is to create a unified platform that connects devotees, priests, and temples, promoting the preservation and practice of Hindu spiritual traditions. We aim to provide authentic information on deities, facilitate temple and priest registrations, and make religious practices accessible, organized, and meaningful for all.</p>
+            <h2>Vision</h2>
+            <p>Our vision is to become the leading digital hub for Hindu spirituality, fostering devotion, knowledge, and community engagement. We aspire to strengthen the bond between devotees and temples while preserving the rich cultural heritage of Hinduism for generations to come.</p>
+
+          </Col>
+          <Col lg={5} md={5} sm={12}>
+            <div className="d-flex justify-content-end">
+              <img src={Missionimg} className="img-fluid" alt="Platform Information" />
+            </div>
+          </Col>
+        </Row>
+
+      </Container></div>
   )
 }
 
