89c0ceea2c46e5075562820d2e857a9250ecc5a6
rajeshthap <jdmjvm@gmail.comgit config user.email>
2025-09-16 15:39:13 +0530

    Dashboard done

--- src/Component/dashboard/innerpage_dashboard/MainDashBoard.js
+++ src/Component/dashboard/innerpage_dashboard/MainDashBoard.js
@@ -21,7 +21,7 @@ const MainDashBoard = () => {
                 <main className="main-container">
                     <div className="content-box">
 
-                        <h1>Dashboard Content</h1>
+                        <h1>Dashboard</h1>
                         <Row>
                             <h2>Online Services</h2>
                             <Col lg={3} md={3} sm={12}>
@@ -84,61 +84,103 @@ const MainDashBoard = () => {
 
                         </Row>
                         <Row>
+                            <h2>Transaction History</h2>
 
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <table class="table">
-                                        <tr class="table-header">
-                                            <th class="cell">S.no</th>
-                                            <th class="cell">Name</th>
-                                            <th class="cell">Username</th>
-                                            <th class="cell">Address</th>
-                                            <th class="cell">Email Id</th>
-                                            <th class="cell">Phone No.</th>
-                                        </tr>
-                                        <tr class="active">
-                                            <td>1</td>
-                                            <td>Smith</td>
-                                            <td>Smith Mark</td>
-                                            <td>#67/B Maxico Usa</td>
-                                            <td>mark@gmail.com</td>
-                                            <td>+3755438764</td>
+                            <div class="col-md-12">
+                                <table class="rwd-table">
+                                    <tbody>
+                                        <tr>
+                                            <th>Supplier Code</th>
+                                            <th>Supplier Name</th>
+                                            <th>Invoice Number</th>
+                                            <th>Invoice Date</th>
+                                            <th>Due Date</th>
+                                            <th>Net Amount</th>
                                         </tr>
                                         <tr>
-                                            <td>2</td>
-                                            <td>Smith Mark</td>
-                                            <td>Smith</td>
-                                            <td>#67/A England</td>
-                                            <td>marksmith@gmail.com</td>
-                                            <td>+376768764</td>
-                                        </tr>
-                                        <tr class="active">
-                                            <td>3</td>
-                                            <td>Willson</td>
-                                            <td>Will</td>
-                                            <td>#3657/34 India New Delhi</td>
-                                            <td>will34@gmail.com</td>
-                                            <td>+372385694</td>
+                                            <td data-th="Supplier Code">
+                                                UPS5005
+                                            </td>
+                                            <td data-th="Supplier Name">
+                                                UPS
+                                            </td>
+                                            <td data-th="Invoice Number">
+                                                ASDF19218
+                                            </td>
+                                            <td data-th="Invoice Date">
+                                                06/25/2016
+                                            </td>
+                                            <td data-th="Due Date">
+                                                12/25/2016
+                                            </td>
+                                            <td data-th="Net Amount">
+                                                $8,322.12
+                                            </td>
                                         </tr>
                                         <tr>
-                                            <td>4</td>
-                                            <td>Jacob</td>
-                                            <td>Jacob Thornton</td>
-                                            <td>#67/B Maxico Usa</td>
-                                            <td>Jacob@gmail.com</td>
-                                            <td>+3755438764</td>
+                                            <td data-th="Supplier Code">
+                                                UPS3449
+                                            </td>
+                                            <td data-th="Supplier Name">
+                                                UPS South Inc.
+                                            </td>
+                                            <td data-th="Invoice Number">
+                                                ASDF29301
+                                            </td>
+                                            <td data-th="Invoice Date">
+                                                6/24/2016
+                                            </td>
+                                            <td data-th="Due Date">
+                                                12/25/2016
+                                            </td>
+                                            <td data-th="Net Amount">
+                                                $3,255.49
+                                            </td>
                                         </tr>
-                                        <tr class="active">
-                                            <td>5</td>
-                                            <td>Larry</td>
-                                            <td>Larry  Bird</td>
-                                            <td>#67/B  USA</td>
-                                            <td>BirdLarry@gmail.com</td>
-                                            <td>+6675438764</td>
+                                        <tr>
+                                            <td data-th="Supplier Code">
+                                                BOX5599
+                                            </td>
+                                            <td data-th="Supplier Name">
+                                                BOX Pro West
+                                            </td>
+                                            <td data-th="Invoice Number">
+                                                ASDF43000
+                                            </td>
+                                            <td data-th="Invoice Date">
+                                                6/27/2016
+                                            </td>
+                                            <td data-th="Due Date">
+                                                12/25/2016
+                                            </td>
+                                            <td data-th="Net Amount">
+                                                $45,255.49
+                                            </td>
                                         </tr>
-                                    </table>
-                                </div>
+                                        <tr>
+                                            <td data-th="Supplier Code">
+                                                PAN9999
+                                            </td>
+                                            <td data-th="Supplier Name">
+                                                Pan Providers and Co.
+                                            </td>
+                                            <td data-th="Invoice Number">
+                                                ASDF33433
+                                            </td>
+                                            <td data-th="Invoice Date">
+                                                6/29/2016
+                                            </td>
+                                            <td data-th="Due Date">
+                                                12/25/2016
+                                            </td>
+                                            <td data-th="Net Amount">
+                                                $12,335.69
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
+
                         </Row>
                     </div>
                 </main>
