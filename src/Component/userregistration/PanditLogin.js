0b6be80e5304e2733054d224be0be0e7c0d8f26f
Shanu Kamboj <shanukamboj98@gmail.com>
2025-09-16 16:09:02 +0530

    navigation done

--- src/Component/userregistration/PanditLogin.js
+++ src/Component/userregistration/PanditLogin.js
@@ -66,7 +66,7 @@ const handleSubmit = async (e) => {
 
       navigate("/MainDashboard");
 
-      navigate("/DashBoard");
+    
 
     } else {
       console.warn("User does not have 'pandit' role, staying on login page.");
