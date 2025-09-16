190bcc2398b7e22859064deec716c28ff20d87ad
rajeshthap <jdmjvm@gmail.comgit config user.email>
2025-09-16 15:18:12 +0530

    favicon

--- src/Component/dashboard/innerpage_dashboard/MainDashBoard.js
+++ src/Component/dashboard/innerpage_dashboard/MainDashBoard.js
@@ -43,7 +43,7 @@ const MainDashBoard = () => {
                                 <Card className="shadow-sm mb-3 bg-body rounded temp-card-bg">
                                     <Card.Body>
                                         <Card.Title className="temp-dash-icon"><FaHandsPraying /></Card.Title>
-                                        <Card.Subtitle className="mb-2 text-muted">Book Puja</Card.Subtitle>
+                                        <Card.Subtitle className="mb-2 text-muted">Donate to Temples</Card.Subtitle>
                                         {/* <Card.Text>
 
                                         </Card.Text> */}
@@ -57,7 +57,7 @@ const MainDashBoard = () => {
                                 <Card className="shadow-sm mb-3 bg-body rounded temp-card-bg">
                                     <Card.Body>
                                         <Card.Title className="temp-dash-icon"><FaHandsPraying /></Card.Title>
-                                        <Card.Subtitle className="mb-2 text-muted">Book Puja</Card.Subtitle>
+                                        <Card.Subtitle className="mb-2 text-muted">Darshan Booking</Card.Subtitle>
                                         {/* <Card.Text>
 
                                         </Card.Text> */}
@@ -71,7 +71,7 @@ const MainDashBoard = () => {
                                 <Card className="shadow-sm mb-3 bg-body rounded temp-card-bg">
                                     <Card.Body>
                                         <Card.Title className="temp-dash-icon"><FaHandsPraying /></Card.Title>
-                                        <Card.Subtitle className="mb-2 text-muted">Book Puja</Card.Subtitle>
+                                        <Card.Subtitle className="mb-2 text-muted">Pooja Booking</Card.Subtitle>
                                         {/* <Card.Text>
 
                                         </Card.Text> */}
@@ -83,7 +83,63 @@ const MainDashBoard = () => {
 
 
                         </Row>
+                        <Row>
 
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <table class="table">
+                                        <tr class="table-header">
+                                            <th class="cell">S.no</th>
+                                            <th class="cell">Name</th>
+                                            <th class="cell">Username</th>
+                                            <th class="cell">Address</th>
+                                            <th class="cell">Email Id</th>
+                                            <th class="cell">Phone No.</th>
+                                        </tr>
+                                        <tr class="active">
+                                            <td>1</td>
+                                            <td>Smith</td>
+                                            <td>Smith Mark</td>
+                                            <td>#67/B Maxico Usa</td>
+                                            <td>mark@gmail.com</td>
+                                            <td>+3755438764</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>Smith Mark</td>
+                                            <td>Smith</td>
+                                            <td>#67/A England</td>
+                                            <td>marksmith@gmail.com</td>
+                                            <td>+376768764</td>
+                                        </tr>
+                                        <tr class="active">
+                                            <td>3</td>
+                                            <td>Willson</td>
+                                            <td>Will</td>
+                                            <td>#3657/34 India New Delhi</td>
+                                            <td>will34@gmail.com</td>
+                                            <td>+372385694</td>
+                                        </tr>
+                                        <tr>
+                                            <td>4</td>
+                                            <td>Jacob</td>
+                                            <td>Jacob Thornton</td>
+                                            <td>#67/B Maxico Usa</td>
+                                            <td>Jacob@gmail.com</td>
+                                            <td>+3755438764</td>
+                                        </tr>
+                                        <tr class="active">
+                                            <td>5</td>
+                                            <td>Larry</td>
+                                            <td>Larry  Bird</td>
+                                            <td>#67/B  USA</td>
+                                            <td>BirdLarry@gmail.com</td>
+                                            <td>+6675438764</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                        </Row>
                     </div>
                 </main>
             </div>
