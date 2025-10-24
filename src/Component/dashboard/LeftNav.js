c503c591ca7f475730ec4990a0d31f733384efad
VAIBHAVTOMAR321 <95899330+VAIBHAVTOMAR321@users.noreply.github.com>
2025-10-24 10:51:43 +0530

    changes

--- src/Component/dashboard/LeftNav.js
+++ src/Component/dashboard/LeftNav.js
@@ -149,7 +149,7 @@ useEffect(() => {
 {
       icon: <GiByzantinTemple />,
       label: "Pandit Booking",
-      path: "/PanditBooking",
+      path: "/PoojaBookingDashBoard",
     
     },
         {
